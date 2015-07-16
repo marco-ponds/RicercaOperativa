720b81edc8f2f8ce77451ff0daf7d223b2d29d1d
Marco Govoni <marcogovo@github.com>
2015-07-16 15:49:38 +0200

    1°versione pseudo-funzionante

--- pick.pl
+++ pick.pl
@@ -3,38 +3,39 @@
 :-lib(cumulative).
 
 
-pick(Starts,Tmov,Tpresa,Velocita,Dimensione):-
-			Npacchi = 2,
-			
-			D = Tmov + Tpresa,
+pick(Pacchi,Tmov,Tpresa,Velocita,Dimensione,Npacchi):-
+		
+			D is Tmov + Tpresa,
 			length(Risorse,Npacchi),
 			length(Durate,Npacchi),
 			length(Pacchi,Npacchi),
 			riempi(Risorse,1),
 			riempi(Durate,D),
 			length(Starts,Npacchi),
+			primo_pacco_start_zero(Starts),
 			Pacchi::1..5,
-			
+
+			%fd_global:alldifferent(Starts),
 			vincolo(Starts,Pacchi,0,Velocita,Dimensione),
-			cumulative([0|Starts],[0|Durate],[0|Risorse],5),
+			cumulative(Starts,Durate,Risorse,5),
 			occurrences(5,Pacchi,Noccur),
 			minimize(labeling(Pacchi),Noccur).
 
 
-vincolo([],[],2,_,_).
+primo_pacco_start_zero([0|_]).
+
+vincolo([],[],_,_,_).
 vincolo([Start|TailStart],[ValPacco|TailPacco],I,Velocita,Dimensione):-
 
-		4*Velocita*Start #>= I*10*4 + (ValPacco-1)*Dimensione,   /*  I*DISTANZA/VELOCITA */
+		5*Velocita*Start #>= I*1*5 + (ValPacco-1)*Dimensione,   /*  I*DISTANZA/VELOCITA */
 	
-		4*Velocita*Start #<= I*10*4 + ValPacco*Dimensione,
-	
-		I1 = I + 1,
+		5*Velocita*Start #<= I*1*5 + ValPacco*Dimensione,
+
+		I1 is I + 1,
 
 		vincolo(TailStart,TailPacco,I1,Velocita,Dimensione).
-
-
-
-
-
+		
+		
+		
 riempi([],_).
 riempi([A|T],A):-riempi(T,A).
